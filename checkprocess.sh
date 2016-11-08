e061fa59273ecc149b0757cef20cf69f8e3880e9
enzoferrari91 <m.lenz1@protonmail.com>
2016-11-08 18:52:34 +0100

    Delete checkprocess.sh

--- checkprocess.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ps cax | grep python2.7 > /dev/null
-if [ $? -eq 0 ]; then
-  echo "$(date) -- Process is running." >> /home/pi/pvmonitor-logs/log_process.txt
-else
-  echo "$(date) -- Process is not running." >> /home/pi/pvmonitor-logs/log_process.txt
-fi
