9b20f4c9d4e6f225145a370c9699797c6cd791cb
enzoferrari91 <Martin@martin-lenz.local>
2016-12-03 10:51:46 +0100

    changes interpol

--- csvdata.py
+++ csvdata.py
@@ -20,6 +20,7 @@ def selectDB(date_from_DB, date_to_DB):
 	cur.execute("SELECT * FROM forecastLog WHERE datetime >= ? and datetime < ?", (date_from_DB, date_to_DB))
 	data = cur.fetchall()
 
+	#cloud = list(zip(*data))[1]
 	ghi = list(zip(*data)[5])
 	timestampList_fcast = list(zip(*data)[0])
 	timestampList_fcast = [str(x) for x in timestampList_fcast]
@@ -38,66 +39,57 @@ def selectDB(date_from_DB, date_to_DB):
 	return (power_pv, timestampList, ghi, timestampList_fcast, ghi_API, timestampList_fcast_API)
 
 
-power_pv, timestampList, ghi, timestampList_fcast, ghi_API, timestampList_fcast_API = selectDB("2016-11-21 00:00","2016-11-24 00:00")
-
-print timestampList_fcast
-
-def interpol(ghi, timestampList_fcast, interval,filename):
+def interpol(tseries, timestamps, interval,filename):
 	# interval 12, 36
-	length = len(ghi)
+	length = len(tseries)
 
-	listeghi = list()
-	listetime = list()
+	timestamps_interpol = list()
+	tseries_interpol = list()
+	output = list()
 
-	listeghi.append(ghi[0])
-	listetime.append(timestampList_fcast[0])
-
+	timestamps_interpol.append(timestamps[0])
+	tseries.append(tseries[0])
 
 	for i in range(0,length-1):
 
 		z = i + 1
 
 		for j in range(1,interval):
-			y = ghi[i] + (ghi[z] - ghi[i]) / interval * j
-			t = datetime.strptime(timestampList_fcast[i],"%Y-%m-%d %H:%M") + timedelta(minutes=5*j)
+			y = tseries[i] + (tseries[z] - tseries[i]) / interval * j
+			t = datetime.strptime(timestamps[i],"%Y-%m-%d %H:%M") + timedelta(minutes=5*j)
 			t = datetime.strftime(t, "%Y-%m-%d %H:%M")
-			listeghi.append(y)
-			listetime.append(t)
+			tseries_interpol.append(y)
+			timestamps_interpol.append(t)
 
-		listeghi.append(ghi[z])
-		listetime.append(timestampList_fcast[z])
+		tseries_interpol.append(tseries[z])
+		timestamps_interpol.append(timestamps[z])
 
-	print("Ausgabe")
-	print(listeghi)
-	print(listetime)
+	output.append(timestamps_interpol)
+	output.append(tseries_interpol)
+	csv = zip(*output)
 
-	outfcast = list()
+	return(csv)
 
-	outfcast.append(listetime)
-	outfcast.append(listeghi)
 
-	csvfcast = zip(*outfcast)
-	print csvfcast
-	return csvfcast
+power_pv, timestampList, ghi, timestampList_fcast, ghi_API, timestampList_fcast_API = selectDB("2016-11-27","2016-11-29")
 
 csvfcast = interpol(ghi, timestampList_fcast,12,"fcast.csv")
 csvfcastAPI = interpol(ghi_API,timestampList_fcast_API,36,"fcastAPI.csv")
 
-with open("outputfcast.csv", "wb") as f:
-    writer = csv.writer(f)
-    writer.writerows(csvfcast)
-with open("outputfcastAPI.csv", "wb") as f:
-    writer = csv.writer(f)
-    writer.writerows(csvfcastAPI)
+with open("fcast.csv", "wb") as f:
+	writer = csv.writer(f)
+	writer.writerows(csvfcast)
 
-
+with open("fcastAPI.csv", "wb") as f:
+	writer = csv.writer(f)
+	writer.writerows(csvfcastAPI)
 
 outpv = list()
 outpv.append(timestampList)
 outpv.append(power_pv)
 csvpv = zip(*outpv)
 
-with open("outputpv.csv", "wb") as f:
+with open("pv.csv", "wb") as f:
     writer = csv.writer(f)
     writer.writerows(csvpv)
 
