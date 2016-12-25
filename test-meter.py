63383b3bc668f83a3c735a9b90bb04a8a0ff5f6e
enzoferrari91 <Martin@w7-bhoelzl.home>
2016-12-25 15:36:55 +0100

    new test update6

--- test-meter.py
+++ test-meter.py
@@ -22,7 +22,31 @@ def read_datablock():
     Request_message='/?!\r\n'
     send(Elster, Request_message, tr)
     print("Sende Request")
-    time.sleep(3)
+    
+    Identification_message=Elster.readline()
+
+    
+    speed = Identification_message[4]
+
+    if (speed == "1"): new_baud_rate = 600
+    elif (speed == "2"): new_baud_rate = 1200
+    elif (speed == "3"): new_baud_rate = 2400
+    elif (speed == "4"): new_baud_rate = 4800
+    elif (speed == "5"): new_baud_rate = 9600
+    elif (speed == "6"): new_baud_rate = 19200
+    else:
+      new_baud_rate = 300
+      speed = "0"
+
+    Acknowledgement_message=ACK + '0' + speed + '0\r\n'
+    send(Elster, Acknowledgement_message, tr)
+    Elster.baudrate=new_baud_rate
+    time.sleep(tr)
+
+
+
+
+    #time.sleep(3)
 
     datablock = ""
     x=Elster.read()
