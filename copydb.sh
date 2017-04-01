1c7520acede5119656ab2814e81940e53159f4ee
enzoferrari91 <Martin@astrid-hp.home>
2017-04-01 11:35:47 +0200

    apache

--- copydb.sh
+++ copydb.sh
@@ -1,3 +1,1 @@
 cp /home/pi/pvmonitor-db/power.db /home/pi/pvmonitor/testdb
-
-
--- restart.sh
+++ restart.sh
@@ -5,7 +5,7 @@ echo "Starte Flask Webserver..."
 cd /home/pi/pvmonitor
 
 # Optional:
-# sudo service apache2 stop
+sudo service apache2 stop
 
 nohup sudo python app.py >/dev/null 2>&1 &
 
