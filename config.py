6d195a3b00ff52456ea99849b4e882293c3be29b
Martin Lenz <martinlenz@Martins-MacBook-Pro.local>
2018-04-09 19:41:11 +0200

    slight changes

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ if platform.system() == "Linux":
     logfilepath = "/home/pi/pvmonitor-logs/"
     webserver = "Raspberry"
 else:
-    dbfilepath = "/Users/Martin/Desktop/pvmonitor/pvmonitor/testdb/power.db"
-    apifilepath = "/Users/Martin/Desktop/pvmonitor/planetosAPI.txt"
-    alarmfilepath = "/Users/Martin/Desktop/pvmonitor/alarm.txt"
+    dbfilepath = "/Users/Martin/Desktop/Python/pvmonitor/pvmonitor/testdb/power.db"
+    apifilepath = "/Users/Martin/Desktop/Python/pvmonitor/planetosAPI.txt"
+    alarmfilepath = "/Users/Martin/Desktop/Python/pvmonitor/alarm.txt"
     webserver = "MacOSX"
 
 # Zwettl
